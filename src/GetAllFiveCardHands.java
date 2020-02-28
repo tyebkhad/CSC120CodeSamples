62bdc8a618ae1cc41e106b99aa58ae987e709715
tyebkhad <tyebkhad@union.edu>
2020-02-28 09:57:13 -0500

    added getallfivecardhands

---
+++ src/GetAllFiveCardHands.java
@@ -0,0 +1,16 @@
+public class GetAllFiveCardHands {
+    /**
+     * with list of letters [a,b,c,d,e,f,g]
+     * to get 5 card combinations
+     * for any combination chosen from this list, the combination either has the first element in the list or it doesn't
+     * therefore some combos include the first and others don't
+     * def combos(list, targetLength):
+     *      if list.length == targetLength
+     *      first = list[0]
+     *      rest = list[1:]
+     *      combosWithoutFirst = combos(rest, targetLength)
+     *      shortCombos = combos(rest, targetLength-1)
+     *      combosWithFirst = list[0] + shortCombos for all in shortCombos
+     *      return combosWithoutFirst and combosWithFirst
+      */
+}
