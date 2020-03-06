ceaedbbe7e7813bc501a3e9181b77571e12f9abe
tyebkhad <tyebkhad@union.edu>
2020-03-06 10:21:31 -0500

    notes on equals method

---
+++ src/equalsMethod.java
@@ -0,0 +1,19 @@
+public class equalsMethod {
+    /**
+     * FOR A DICE CLASS
+     * @Override
+     * public boolean equals(Object other){
+     *     if (other == null) {
+     *         return false;
+     *     } else if (other == this) {
+     *         return true;
+     *     } else if (!(other instanceOf Dice)) {
+     *         return false;
+     *     }
+     *
+     *     Dice otherDice = (Dice) other;
+     *
+     *     return other.currentValue == this.currentValue && other.numSides == this.numSides
+     * }
+     */
+}
