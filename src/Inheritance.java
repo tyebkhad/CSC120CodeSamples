00082e00b31934bfb65c6a5b9db0261daee34271
tyebkhad <tyebkhad@union.edu>
2020-03-09 10:16:25 -0400

    notes on interfaces

---
+++ src/Inheritance.java
@@ -0,0 +1,10 @@
+public class Inheritance {
+    /**
+     * dispatch rule - type of object the method is being called on determines which method to call
+     * Java method dispatch  - determined by the dynamic type of the object the method is called on and the static
+     *                         types of the parameters passed
+     * dynamic - determined at runtime
+     * static - declared in the code
+     * polymorphism -
+     */
+}
